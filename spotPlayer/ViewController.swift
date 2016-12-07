f53cabec748b675e23fef9d68571912b11bade99
Giovanni Martins <giovannimartins2000@gmail.com>
2016-12-07 11:15:12 -0200

    putting images on the tracks

--- spotPlayer/ViewController.swift
+++ spotPlayer/ViewController.swift
@@ -9,11 +9,18 @@
 import UIKit
 import Alamofire
 
+
+struct post {
+    let mainImage : UIImage!
+    let name : String!
+    
+}
+
 class TableViewController: UITableViewController {
     
-    var names = [String]()
+    var posts = [post]()
     
-    var searchURL = "https://api.spotify.com/v1/search?q=Frank+Sinatra&type=track"
+    var searchURL = "https://api.spotify.com/v1/search?q=Shawn+Mendes&type=track"
     
     typealias JSONStandard = [String : AnyObject]
     
@@ -36,15 +43,23 @@ class TableViewController: UITableViewController {
                 if let items = tracks["items"] as? [JSONStandard]{
                     for i in 0..<items.count{
                         let item = items[i]
-                        
+                        print(item)
                         let name = item["name"] as! String
-                        names.append(name)
                         
-                        self.tableView.reloadData()
+                        if let album = item["album"] as? JSONStandard{
+                            if let images = album["images"] as? [JSONStandard]{
+                                let imageData = images[0]
+                                let mainImageURL = URL(string: imageData["url"] as! String)
+                                let mainImageData = NSData(contentsOf: mainImageURL!)
+                                
+                                let mainImage = UIImage(data: mainImageData as! Data)
+                                
+                                posts.append(post.init(mainImage: mainImage, name: name))
+                                self.tableView.reloadData()
+                            }
+                        }
                     }
-                    
                 }
-                
             }
         }
         catch{
@@ -54,13 +69,19 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return posts.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
         
-        cell?.textLabel?.text = names[indexPath.row]
+        let mainImageView = cell?.viewWithTag(2) as! UIImageView
+        
+        mainImageView.image = posts[indexPath.row].mainImage
+        
+        let mainLabel = cell?.viewWithTag(1) as! UILabel
+        
+        mainLabel.text = posts[indexPath.row].name
         
         return cell!
     }
